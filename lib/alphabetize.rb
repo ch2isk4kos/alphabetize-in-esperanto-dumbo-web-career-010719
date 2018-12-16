9fccf231288f7e753558929d4cb7f6c33eba5cb1
Chris Kakos <ckakos@gmail.com>
2018-12-16 23:39:39 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,21 @@
 def alphabetize(arr)
-  # code here
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
-  word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
-  word_hash = set_word_hash_values(a_to_z_hash, word_hash) #sets values of word hash to array of #'s'
-  words = sort_words(word_hash) #sorts words
- end
+  finished = false
+  i = 0
+  while !finished
+    
+    if i != arr.length - 1
+      str1 = arr[i]
+      str2 = arr[i + 1]
+      if should_be_swaped?(str1, str2)
+        arr[i] = str2
+        arr[i + 1] = str1
+      end
+      i += 1
+    else
+      finished = check_arr(arr)
+      i = 0
+    end
+
+  end
+  arr
+end
