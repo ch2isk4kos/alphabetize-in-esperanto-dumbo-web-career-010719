3b5d43bdb8da851b5f89b9fd68f4e61332f2d358
Chris Kakos <ckakos@gmail.com>
2018-12-16 23:39:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,8 @@
 def alphabetize(arr)
-    # code here
-    arr.sort_by! do |a|
-        positions = []
-
-        a.each_char do |char|
-            positions << ESPERANTO_ALPHABET.index(char)
-        end
-        positions
-    end
-    arr
-end
+  # code here	
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  a_to_z_hash = alpha_hash(alphabet) #turns alphabet into hash with values as numbers
+  word_hash = make_word_hash(arr) #creates hash from words to be alphabetized.
+  word_hash = set_word_hash_values(a_to_z_hash, word_hash) #sets values of word hash to array of #'s'
+  words = sort_words(word_hash) #sorts words
+ end
